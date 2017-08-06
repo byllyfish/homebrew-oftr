9a98b1e47706eff9e80a72429c0c05d3683d2a63
Bill Fisher <william.w.fisher@gmail.com>
2017-08-05 23:48:37 -0700

    Make test do something productive

--- oftr.rb
+++ oftr.rb
@@ -3,7 +3,7 @@ class Oftr < Formula
   homepage "https://github.com/byllyfish/oftr"
   url "https://launchpad.net/~byllyfish/+archive/ubuntu/oftr/+files/oftr_0.32.0.orig.tar.gz"
   sha256 "276cc6b4aa3d98aafa1d638ecd03f6ea274afc9482a33d574548331c79be095f"
-  head "https://github.com/byllyfish/oftr.git", :branch => :master
+  head "https://github.com/byllyfish/oftr.git"
 
   depends_on "cmake" => :build
   depends_on "go" => :build
@@ -18,6 +18,7 @@ class Oftr < Formula
   end
 
   test do
-    system "#{bin}/oftr", "--version"
+    (testpath/"hello.yml").write("type: HELLO")
+    system "#{bin}/oftr", "encode", "--hex", "--ofversion=4", "hello.yml"
   end
 end
