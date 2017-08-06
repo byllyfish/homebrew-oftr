d4565ba645db6c3a3576e53e4b8b3cede14bd862
Bill Fisher <william.w.fisher@gmail.com>
2017-08-05 23:10:27 -0700

    Add stable.

--- oftr.rb
+++ oftr.rb
@@ -1,15 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Oftr < Formula
   desc "OpenFlow to YAML Translator and Microservice"
   homepage "https://github.com/byllyfish/oftr"
-  head "https://github.com/byllyfish/oftr", :using => :git, :branch => :master
+
+  head "https://github.com/byllyfish/oftr.git", 
+        :branch => :master
+
+  stable "https://github.com/byllyfish/oftr.git", 
+        :tag => "v0.32.0",
+        :revision => "cfce9c7f11a25ba2b55677afc771c057bfd73caf"
+
+  #devel "https://github.com/byllyfish/oftr.git", 
+  #      :branch => :develop
 
   depends_on "cmake" => :build
   depends_on "go" => :build
-  
+
   def install
     args = std_cmake_args
 
@@ -20,15 +25,6 @@ class Oftr < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test oftr`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "#{bin}/oftr", "--version"
   end
 end
