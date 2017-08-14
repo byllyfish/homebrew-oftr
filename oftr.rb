05799b455a389815e255fa11c927e7f7bf890f75
Bill Fisher <william.w.fisher@gmail.com>
2017-08-14 00:39:50 -0700

    Update bottle hash.

--- oftr.rb
+++ oftr.rb
@@ -8,8 +8,7 @@ class Oftr < Formula
   bottle do
     root_url "https://dl.bintray.com/byllyfish/bottles"
     cellar :any_skip_relocation
-    rebuild 1
-    sha256 "1b08c344d260a4b40bb6de8c140acb04b1909534e0c0cc85b4f0c7e174399320" => :sierra
+    sha256 "d64a80ad4a741b86e303870d9a28b01be31ec98a2c50ea63d355023edb46ee9e" => :sierra
   end
 
   depends_on "cmake" => :build
