5f4cc53afb0438f01d2971848873160f6af77682
Bill Fisher <william.w.fisher@gmail.com>
2017-10-15 20:03:35 -0700

    Bump version.

--- oftr.rb
+++ oftr.rb
@@ -1,8 +1,8 @@
 class Oftr < Formula
   desc "OpenFlow to YAML Translator and Microservice"
   homepage "https://github.com/byllyfish/oftr"
-  url "https://launchpad.net/~byllyfish/+archive/ubuntu/oftr/+files/oftr_0.37.0.orig.tar.gz"
-  sha256 "a24a9d42d2d70ced31e972d4f40d72d20ef6247659a1e21866de7bc59cec7a0b"
+  url "https://launchpad.net/~byllyfish/+archive/ubuntu/oftr/+files/oftr_0.38.0.orig.tar.gz"
+  sha256 "8afe1129b8e3b9a2aa5297dd27997986376405a16622719655545b80ae7210ea"
   head "https://github.com/byllyfish/oftr.git"
 
   bottle do
