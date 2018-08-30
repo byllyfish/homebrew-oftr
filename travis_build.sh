f3c7f288da6fa710d3547c707ad7525d407be54d
Bill Fisher <william.w.fisher@gmail.com>
2018-08-30 13:15:31 -0700

    Add travis_build.sh script.
    Rename “—“ to “-“ in bottle name.

---
+++ travis_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+ROOT_URL="https://github.com/byllyfish/homebrew-oftr/releases/download/$TRAVIS_TAG"
+
+brew tap byllyfish/oftr
+brew update
+
+# Use travis_wait; building oftr may take longer than 10 minutes.
+
+travis_wait brew install --build-bottle oftr
+brew bottle --no-rebuild --root-url="$ROOT_URL" oftr
+
+# Homebrew introduced a change which puts "--" in the bottle name. Rename
+# the bottle to something that homebrew expects to download/install.
+
+for bottle in *.bottle.tar.gz; do
+  mv "$bottle" "${bottle/--/-}"
+done
