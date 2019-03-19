30d43d474aa89224519df3255a9fce3341757342
Bill Fisher <william.w.fisher@gmail.com>
2019-03-18 22:50:57 -0700

    Update bottle.

--- oftr.rb
+++ oftr.rb
@@ -6,9 +6,9 @@ class Oftr < Formula
   head "https://github.com/byllyfish/oftr.git"
 
   bottle do
-    root_url "https://github.com/byllyfish/homebrew-oftr/releases/download/0.56.0"
+    root_url "https://github.com/byllyfish/homebrew-oftr/releases/download/0.57.0"
     cellar :any_skip_relocation
-    sha256 "aa5871bf116ba8e1fa616a031a1cc6bfbd19fdc0bd70fd94ec166543daabfd2a" => :high_sierra
+    sha256 "121676e36a84fef095a00a62141e3d8ecb9dc1b73ea7e5fa5c91ebda5c3c9154" => :high_sierra
   end
 
   depends_on "cmake" => :build
